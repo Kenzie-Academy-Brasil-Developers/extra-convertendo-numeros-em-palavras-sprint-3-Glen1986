ad0c8be243ec7cc3597ef3b339d0f1f14febec74
glen1986 <escobar.glen@gmail.com>
2021-07-16 19:08:57 -0300

    1 a 999

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ const units = [
   " One",
   " Two",
   " Tree",
-  " For",
+  " Four",
   " Five",
   " Six",
   " Seven",
-  " Eigth",
+  " Eight",
   " Nine",
   " Ten",
   " Eleven",
   " Twelve",
   " Thirteen",
-  " Forteen",
-  " Fivteen",
+  " Fourteen",
+  " Fifteen",
   " Sixteen",
   " Seventeen",
   " Eighteen",
@@ -28,28 +28,28 @@ const units = [
 ];
 const decenas = [
   "",
-  " ten",
-  " twenty",
-  " thirty",
-  " forty",
-  " fifty",
-  " sixty",
-  " seventy",
-  " eighty",
-  " ninety",
+  " Ten",
+  " Twenty",
+  " Thirty",
+  " Forty",
+  " Fifty",
+  " Sixty",
+  " Seventy",
+  " Eighty",
+  " Ninety",
 ];
 
 const cents = [
   "",
-  " oneHundred",
-  " twoHundred",
-  " threeHundred",
-  " forHundred",
-  " fiveHundred",
-  " sixHundred",
-  " sevenHundred",
-  " eightHundred",
-  " nineHunred",
+  " OneHundred",
+  " TwoHundred",
+  " ThreeHundred",
+  " ForHundred",
+  " FiveHundred",
+  " SixHundred",
+  " SevenHundred",
+  " EightHundred",
+  " NineHunred",
 ];
 let inp = 0;
 let input = 0;
@@ -60,24 +60,201 @@ button.addEventListener("click", function () {
   for (let i = 0; i < valores; i++) {
     inp = i + 1;
     input = inp.toString();
-    if(input < 20){
+    if(input <= 20){
     let unidad = units.at(input);
     frase.push(unidad);
     conteudo.innerText = frase;
     }
-    if (input > 20 && input < 99) {
+    if (input > 20 && input <= 99) {
       let idex = input.toString().split("");
       let decena = decenas.at(idex[0]);
       let unidad = units.at(idex[1]);
       let totalUnidades = `${decena}` + `${unidad}`;
-      //let totalUnidades =[];
-      //  totalUnidades.push(decena);//`${decena}` + `${unidad}`;
-      //  totalUnidades.push(unidad);//`${decena}` + `${unidad}`;
-      
       frase.push(totalUnidades);
       conteudo.innerText = frase;
-      console.log(totalUnidades);
     }
+    if(input > 99 && input <= 110){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+    if(input > 110 && input <= 120){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+
+     /* let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      */ let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+      }
+   if(input > 120 && input <= 210){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+    if(input > 210 && input <= 220){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+    ///////////////////////////////////////////////
+   if(input > 220 && input <= 310){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 310 && input <= 320){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+///////////////////////////////////////////////
+   if(input > 320 && input <= 410){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 410 && input <= 420){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+///////////////////////////////////////////////
+   if(input > 420 && input <= 510){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 510 && input <= 520){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+///////////////////////////////////////////////
+   if(input > 520 && input <= 610){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 610 && input <= 620){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+    ///////////////////////////////////////////////
+   if(input > 620 && input <= 710){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 710 && input <= 720){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+    ///////////////////////////////////////////////
+   if(input > 720 && input <= 810){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 810 && input <= 820){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+///////////////////////////////////////////////
+   if(input > 720 && input <= 810){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
+ if(input > 910 && input <= 920){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena =  idex[1].concat(idex[2])   
+      let decenas = units.at(decena)    
+      let totalCentenas = `${centena}` +`${decenas}` ;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
+///////////////////////////////////////////////
+   if(input > 920 && input <= 1000){
+      let idex = input.toString().split("");
+      let centena = cents.at(idex[0]);
+      let decena = decenas.at(idex[1]);
+      let unidad = units.at(idex[2]);
+      let totalCentenas = `${centena}` +`${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+       }
   }
   /*  if (input > 99 && input < 120) {
     let idex = input.toString().split("");
