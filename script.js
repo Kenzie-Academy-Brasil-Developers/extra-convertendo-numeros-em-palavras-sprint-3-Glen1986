402783ca47b03148014b7fec04a8ca4c7011d9bd
glen1986 <escobar.glen@gmail.com>
2021-07-19 16:57:36 -0300

    1 a 1010

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const units = [
   "",
   " One",
   " Two",
-  " Tree",
+  " Three",
   " Four",
   " Five",
   " Six",
@@ -51,6 +51,11 @@ const cents = [
   " EightHundred",
   " NineHunred",
 ];
+const miles = [
+  "",
+  " OneThousand",
+];
+
 let inp = 0;
 let input = 0;
 let frase = [];
@@ -255,225 +260,16 @@ button.addEventListener("click", function () {
       frase.push(totalCentenas);
       conteudo.innerText = frase;
        }
+    if(input > 999 && input <= 1010){
+      let idex = input.toString().split("");
+      let mil = miles.at(idex[0]);
+      let centena = cents.at(idex[1]);
+      let decena = decenas.at(idex[2]);
+      let unidad = units.at(idex[3]);
+      let totalCentenas = `${mil}` + `${centena}` + `${decena}` + `${unidad}`;
+      frase.push(totalCentenas);
+      conteudo.innerText = frase;
+    }
   }
-  /*  if (input > 99 && input < 120) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    frase.push(totalCent);
-    console.log(totaCent);
-    conteudo.innerText = frase;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 120 && input < 199) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  /*if (input > 199 && input <= 220) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 220 && input < 299) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 299 && input <= 320) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 320 && input < 399) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 399 && input <= 420) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 420 && input < 499) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 499 && input <= 520) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 520 && input < 599) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 599 && input <= 620) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 620 && input < 699) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 699 && input <= 720) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 720 && input < 799) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 799 && input <= 820) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 820 && input < 899) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 899 && input <= 920) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let valorDecena = idex[1].concat(idex[2]);
-    let decena = units.splice(valorDecena, 1);
-    let totalCent = `${centena}` + `${decena}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-  if (input > 920 && input < 999) {
-    let idex = input.toString().split("");
-    //   console.log(idex);
-    let centena = cents.splice(idex[0], 1);
-    let decena = decenas.splice(idex[1], 1);
-    let unidad = units.splice(idex[2], 1);
-    let totalCent = `${centena}` + `${decena}` + `${unidad}`;
-    //console.log(totalCent);
-    conteudo.innerText = totalCent;
-    //conteudo.innerText = `${decena}` + `${unidad}`;
-    //conteudo.innerText = total;
-  }
-
-  button.disabled = true;*/
 });
 //`${decens[i]}`
